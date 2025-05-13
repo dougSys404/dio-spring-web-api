f8923c4cd5b13ab53018a665c3e40116973614e6
Doug <douglas@macOs.local>
2025-05-13 11:00:15 -0300

    First commit

---
+++ src/main/java/spring_web_api/DioSpringWebApiApplication.java
@@ -0,0 +1,13 @@
+package spring_web_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSpringWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSpringWebApiApplication.class, args);
+	}
+
+}
